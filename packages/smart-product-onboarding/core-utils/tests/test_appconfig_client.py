5c4948983f6c65a72b5c4f3ff126dc6f5d1b6c39
Jeff Strunk <strunkjd@amazon.com>
2026-03-27 11:19:24 -0300

    chore: fix license header

--- packages/smart-product-onboarding/core-utils/tests/test_appconfig_client.py
+++ packages/smart-product-onboarding/core-utils/tests/test_appconfig_client.py
@@ -1,6 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
-# Licensed under the Amazon Software License  http://aws.amazon.com/asl/
+# SPDX-License-Identifier: MIT-0
 
 """Unit tests for the AppConfig configuration client.
 
